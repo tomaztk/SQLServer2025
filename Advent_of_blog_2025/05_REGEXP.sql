363b0163116d2861ed60a53896c4e38cbc8cc647
tomazkastrun <tomazkastrun@sqlserver2025>
2025-12-07 17:15:48 +0000

    Adding REGEXP

--- Advent_of_blog_2025/05_REGEXP.sql
+++ Advent_of_blog_2025/05_REGEXP.sql
@@ -15,7 +15,11 @@ USE db_05_regex;
 GO
 
 
+/*
 
+REGEXP_LIKE 
+
+*/
 
 DROP TABLE IF EXISTS EMPLOYEES;
 GO
@@ -46,3 +50,50 @@ GO
 
 
 
+/*
+
+REGEXP_SUBSTR
+-- Returns one occurrence of a substring of a string that matches the regular expression pattern.
+If no match is found, it returns NULL.
+*/
+
+USE db_05_regex;
+GO
+
+INSERT INTO EMPLOYEES ([Name], Email, Phone_Number)
+VALUES ('Orange Blue', 'Orage.blue@fruits.co.uk', '(040) 456-7890');
+GO
+
+INSERT INTO EMPLOYEES ([Name], Email, Phone_Number)
+VALUES ('Tomato Green', 'tomato@gov.uk.com', '(080) 431-9760');
+GO
+
+SELECT REGEXP_SUBSTR(EMAIL, '@(.+)$', 1, 1, 'i', 1) AS DOMAIN
+FROM EMPLOYEES
+
+
+INSERT INTO EMPLOYEES ([Name], Email, Phone_Number)
+VALUES ('Tomato Slovenian', 'tomato@gov.si', '(080) 431-9760');
+GO
+
+SELECT REGEXP_SUBSTR(EMAIL, '@(.+)$', 1, 1, 'i', 1) AS DOMAIN
+FROM EMPLOYEES
+
+-- No Match
+-- later we get match
+SELECT 
+   REGEXP_SUBSTR(EMAIL, '@(.+)si',1,1, 'i', 0) AS email_domain
+FROM EMPLOYEES
+
+
+
+/*
+
+REGEXP_REPLACE()
+-- Returns a modified source string replaced by a replacement string, where the occurrence of the regular expression pattern found.
+If no matches are found, the function returns the original string.
+*/
+
+-- obfuscate the last four digits
+SELECT REGEXP_REPLACE(PHONE_NUMBER, '\d{4}$', '****') AS PHONE_OBFUSCATE
+FROM EMPLOYEES;
