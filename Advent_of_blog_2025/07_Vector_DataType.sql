05befc49ea8fb3c88a8e7109cf3db28b94877fc8
tomazkastrun <tomazkastrun@sqlserver2025>
2025-12-14 15:16:57 +0000

    update Vector data

--- Advent_of_blog_2025/07_Vector_DataType.sql
+++ Advent_of_blog_2025/07_Vector_DataType.sql
@@ -180,3 +180,50 @@ DECLARE @v6 AS VECTOR(4) = '[1.043,2043.1234,-205.043, 0.000034234]';
 SELECT 
    VECTORPROPERTY(@v6, 'Dimensions') AS VectorDimension
   ,VECTORPROPERTY(@v6 ,'BaseType') AS VectorType
+
+
+
+  /*
+
+  AI_GENERATE_EMBEDDINGS
+
+  */
+
+use [db_11_vector];
+GO
+
+GRANT EXECUTE ANY EXTERNAL ENDPOINT TO dbo;
+GO
+
+
+-- 1) Create a DMK if it doesn't exist (pick a strong password you can keep safe)
+IF NOT EXISTS (SELECT 1 FROM sys.symmetric_keys WHERE name = '##MS_DatabaseMasterKey##')
+BEGIN
+  CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Very_Strong_P@ssw0rd!_Keep_Safe';
+END
+GO
+
+
+
+IF NOT EXISTS (SELECT 1 FROM sys.database_scoped_credentials WHERE name = 'https://my_location.cognitiveservices.azure.com]')
+BEGIN
+  CREATE DATABASE SCOPED CREDENTIAL [https://my_location.cognitiveservices.azure.com]
+  WITH IDENTITY = 'HTTPEndpointHeaders',
+   SECRET   = '{"api-key":"My_Foundry_API_Key_Secrets"}';
+END
+GO
+
+CREATE EXTERNAL MODEL MyFoundryEmbeddingModel
+WITH (
+      LOCATION = 'https://my_location/openai/deployments/text-embedding-ada-002/embeddings?api-version=2023-05-15',
+      API_FORMAT = 'Azure OpenAI',
+      MODEL_TYPE = EMBEDDINGS,
+      MODEL = 'text-embedding-ada-002',
+      CREDENTIAL = [https://my_location.cognitiveservices.azure.com]
+);
+
+
+SELECT id
+     ,content
+     ,AI_GENERATE_EMBEDDINGS(content USE MODEL MyFoundryEmbeddingModel) AS AoC_Embeddings
+from [db_11_vector].dbo.Aoc_days;
